7f9d015421839324b7ca65348a401a68c2d2b5d4
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-01 17:02:03 +0900

    Tunaeffect

---
+++ Assets/Scripts/MakeTuna.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTuna : MonoBehaviour
+{
+    public GameObject tuna;
+    void Start()
+    {
+        InvokeRepeating("maketuna", 0.0f, 1.0f);
+    }
+    void maketuna()
+    {
+        Instantiate(tuna);
+    }
+
+}
---
+++ Assets/Scripts/Tuna.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuna : MonoBehaviour
+{
+    float speed = 0.005f;
+
+    void Start()
+    {
+        float x = 4.5f;
+        float y = Random.Range(5.0f, -5.0f);
+        transform.position = new Vector2(x, y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed;
+        if (transform.position.x < -4.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
