f5f05e12c727338a257ca80770af8f1e6b93a27f
SoKH-creator <skh7235@naver.com>
2025-07-01 14:49:41 +0900

    Suffle Card
    Add Card suffling in Borad.cs
    All face photos are renamed.

--- Assets/Scripts/Borad.cs
+++ Assets/Scripts/Borad.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Borad : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class Borad : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+        arr = arr.OrderBy(X => Random.Range(0f, 5f)).ToArray();
+
         for (int i = 0; i < 12; i++)
         {
             GameObject go = Instantiate(card, this.transform);
@@ -17,6 +21,8 @@ public class Borad : MonoBehaviour
             float y = (i / 3) * 2.0f - 3.6f;
 
             go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i]);
+
         }
     }
 
