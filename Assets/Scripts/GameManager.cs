b8843b06a7ff4fc98382c702bf1ad9822a05df34
DMDZ99 <ldhdo430@naver.com>
2025-07-01 12:10:36 +0900

    GameManager1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Text timeTxt;
+    public GameObject endTxt;
+    float time = 0.0f;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        if (time > 30.0f)
+        {
+            Time.timeScale = 0.0f;
+            endTxt.SetActive(true);
+        }
+}
