eda955f73b05ca0a65f457f739148d876e7c220d
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-01 10:13:13 +0900

    Initial commit

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
