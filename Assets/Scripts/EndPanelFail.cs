1088b6f03321ebc0c671e36691134fc83ab90ef1
xonxseo <jsg8369@naver.com>
2025-07-01 18:54:41 +0900

    Update EndPanelFail.cs

--- Assets/Scripts/EndPanelFail.cs
+++ Assets/Scripts/EndPanelFail.cs
@@ -3,27 +3,30 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndPanelFail : MonoBehaviour
+{
+
 
     private GameObject endPanelInstance;
 
-public void ShowEndRanel()
-{
-    if(endPanelInstance == null)
+    public void ShowEndRanel()
     {
-        GameObject panelPrefab = Resources.Load<GameObject>
+        if (endPanelInstance == null)
+        {
+            GameObject panelPrefab = Resources.Load<GameObject>("Prefabs/EndPanelFail");
+        }
     }
-}
 
-{
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
