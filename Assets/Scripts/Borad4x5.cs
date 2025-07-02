a01a1afcd7ad8a7c303ba7042cd38ceb44b54d03
SoKH-creator <skh7235@naver.com>
2025-07-02 17:32:33 +0900

    Add StageScene3_4x5

---
+++ Assets/Scripts/Borad4x5.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Borad4x3 : MonoBehaviour
+{
+    public GameObject card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.SetCardCount(12);
+
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+        arr = arr.OrderBy(X => Random.Range(0f, 5f)).ToArray();
+
+        for (int i = 0; i < 12; i++)
+        {
+            GameObject go = Instantiate(card, this.transform);
+
+            float x = (i % 3) * 1.75f - 1.75f;
+            float y = (i / 3) * 2.0f - 3.6f;
+
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().Setting(arr[i]);
+            go.GetComponent<Card>().Deal (i);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
