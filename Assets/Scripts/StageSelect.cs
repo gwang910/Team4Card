3cc6d64ef909a06d67e8564ebfcfc8015e08bb54
DMDZ99 <ldhdo430@naver.com>
2025-07-02 15:50:53 +0900

    btn,lock1

---
+++ Assets/Scripts/StageSelect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour
+{
+    public Button stage4x4Button;
+    public Button stage4x5Button;
+    public GameObject lockIcon4x4;
+    public GameObject lockIcon4x5;
+
+    private void Start()
+    {
+        int isCleared = PlayerPrefs.GetInt
+    }
+    public void MainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadStage4x4()
+    {
+        SceneManager.LoadScene("Stage4x4Scene");
+    }
+    public void LoadStage4x5()
+    {
+        SceneManager.LoadScene("Stage4x5Scene");
+    }
+}
