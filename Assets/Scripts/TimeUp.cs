57e276c43d4c26870586c5e3204b06221cce0a12
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-02 15:43:13 +0900

    timeupfix

--- Assets/Scripts/TimeUp.cs
+++ Assets/Scripts/TimeUp.cs
@@ -9,17 +9,21 @@ public class TimeUp : MonoBehaviour
     public AudioClip TimeUpSound;
     public Animator animator;
 
+    private bool wasTimeUp = false;
+
     void Update()
     {
 
         bool isTimeUp = animator.GetBool("TimeUp");
 
-        if (isTimeUp == true)
+        if (isTimeUp && !wasTimeUp)
         {
             bgmSource.Stop();
             bgmSource.clip = this.TimeUpSound; // 시간임박 음악 재생
             bgmSource.Play();
         }
+
+        wasTimeUp = isTimeUp;
 
     }
 
