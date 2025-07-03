498554d20c7a437b77a36dc7f41a8a2dd7668c10
DMDZ99 <ldhdo430@naver.com>
2025-07-03 14:02:01 +0900

    lockanim2

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -16,16 +16,28 @@ public class StageSelect : MonoBehaviour
 
     private void Start()
     {
+        int stage2_4x4Unlock = PlayerPrefs.GetInt("Stage2_4x4_unlock", 0);      // unlock or lock?
+        int stage3_4x5Unlock = PlayerPrefs.GetInt("Stage3_4x5_unlock", 0);
+
         lockAnimator4x4 = lockIcon4x4.GetComponent<Animator>();
         lockAnimator4x5 = lockIcon4x5.GetComponent<Animator>();
 
-        int stage2_4x4Unlock = PlayerPrefs.GetInt("Stage2_4x4_unlock", 0);
-        int stage3_4x5Unlock = PlayerPrefs.GetInt("Stage3_4x5_unlock", 0);
-
-        if(stage2_4x4Unlock == 1)
+        if (stage2_4x4Unlock == 1)
         {
             stage2_4x4Button.interactable = true;
-            lockIcon4x4.SetActive(false);
+
+            if (PlayerPrefs.GetString("NewlyUnlocked","") == "stage2_4x4")
+            {
+                lockIcon4x4.SetActive(true);
+                if (lockAnimator4x4 != null)
+                {
+                    lockAnimator4x4.SetTrigger("FadeOut");      //Only one animation do
+                }
+            }
+            else
+            {
+                lockIcon4x4.SetActive(true);
+            }
         }
         else
         {
@@ -36,13 +48,28 @@ public class StageSelect : MonoBehaviour
         if (stage3_4x5Unlock == 1)
         {
             stage3_4x5Button.interactable = true;
-            lockIcon4x5.SetActive(false);
+
+
+            if (PlayerPrefs.GetString("NewlyUnlocked", "") == "stage3_4x5")
+            {
+                lockIcon4x5.SetActive(true);
+                if (lockAnimator4x5 != null)
+                {
+                    lockAnimator4x5.SetTrigger("FadeOut");      //Only one animation do
+                }
+            }
+            else
+            {
+                lockIcon4x5.SetActive(true);
+            }
         }
         else
         {
             stage3_4x5Button.interactable = false;
             lockIcon4x5.SetActive(true);
         }
+        PlayerPrefs.DeleteKey("NewlyUnlocked");         // if dont deletekey > stageclear > animation always
+        PlayerPrefs.Save();
     }
     public void LoadStage4x3()
     {
