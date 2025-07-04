293fb98ad49f666761a62398bc5e000bfda313e8
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-04 09:59:46 +0900

    jusuk

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -55,20 +55,22 @@ public class AudioManager : MonoBehaviour
             }
         }
 
-        if (animator != null)
+        if (animator != null) // ‘animator 오브젝트가 지정되었을때’의 조건문.
         {
-            bool isTimeUp = animator.GetBool("TimeUp");
+            bool isTimeUp = animator.GetBool("TimeUp"); // 스크립트 내 isTimeUp의 bool값을 animator 오브젝트의 “TimeUp”의 bool값에서 끌어와서 지정
 
 
-            if (isTimeUp && !wasTimeUp)
+            if (isTimeUp && !wasTimeUp) // isTimeUp의 bool값이 wasTimeUp의 bool값과 다를 때의 조건문.
+                                        // wasTimeUp은 이 코드 이전에 false로 지정함.
+                                        // 즉, isTimeUp의 bool값이 true일때의 조건문이 됨.
             {
-                bgmSource.Stop();
-                bgmSource.clip = this.TimeUpSound; // 시간임박 음악 재생
-                bgmSource.Play();
-                bgmSource.loop = true;
+                bgmSource.Stop(); // 재생중이던 배경음악 정지
+                bgmSource.clip = this.TimeUpSound; // 제한시간 임박 시의 배경음악으로 전환
+                bgmSource.Play(); // 배경음악 재생
+                bgmSource.loop = true; // 배경음악이 끝났을 때, 반복해서 재생되게 함.
             }
 
-            wasTimeUp = isTimeUp;
+            wasTimeUp = isTimeUp; // wasTimeUp의 bool값을 true로 지정함으로써 현재 상태 저장(음악 중복재생 방지)
         }
 
 
